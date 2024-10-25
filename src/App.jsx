06cbc74208007edb7d72d8284f5aa618ac5be056
shane <95544653+shanecash@users.noreply.github.com>
2024-10-25 10:00:19 -0400

    add manifest error handling

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,66 @@ const handleListener = fromCallback(({ sendBack, receive }) => {
 
   receive((event) => {
     if (event.type === "start") {
+      const src =
+        "https://bcovlive-a.akamaihd.net/1ad942d15d9643bea6d199b729e79e48/us-east-1/6183977686001/profile_1/chunklist.m3u8";
+
       var video = document.getElementById("video");
 
       var hls = new Hls();
 
-      hls.loadSource("https://fe.tring.al/delta/105/out/u/rdghfhsfhfshs.m3u8");
+      hls.loadSource(src);
 
       hls.attachMedia(video);
 
-      video.play();
+      Object.values(Hls.Events).forEach((hlsEvent) => {
+        hls.on(hlsEvent, function (event, data) {
+          console.log(event);
+          console.log(`HLS Event: ${hlsEvent}`, data);
+        });
+      });
 
-      hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-        console.log("video and hls.js are now bound together !");
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        var errorType = data.type;
+        var errorDetails = data.details;
+        var errorFatal = data.fatal;
+
+        switch (data.details) {
+          case Hls.ErrorDetails.MANIFEST_LOAD_ERROR:
+            console.error(src, "manifest load error");
+            sendBack({ type: "MANIFEST_LOAD_ERROR" });
+            break;
+          case Hls.ErrorDetails.MANIFEST_LOAD_TIMEOUT:
+            console.error(src, "manifest load timeout");
+            sendBack({ type: "MANIFEST_LOAD_TIMEOUT" });
+            break;
+          case Hls.ErrorDetails.MANIFEST_PARSING_ERROR:
+            console.error(src, "manifest parsing error");
+            sendBack({ type: "MANIFEST_PARSING_ERROR" });
+            break;
+          default:
+            break;
+        }
+      });
+
+      /*
+      hls.on(Hls.Events.BUFFER_APPENDED, function () {
+        console.log("Data appended to buffer.");
+      });
+
+      hls.on(Hls.Events.BUFFER_STALLED, function () {
+        console.log("Buffer is stalled, waiting for data...");
+      });
+
+      hls.on(Hls.Events.BUFFER_APPENDING, function () {
+        console.log("Appending data to buffer...");
+      });
+      */
+
+      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+        console.log(
+          "Manifest loaded, found " + data.levels.length + " quality levels"
+        );
+        video.play();
       });
     }
 
@@ -59,7 +107,7 @@ const hlsMachine = setup({
           {
             guard: "isSupported",
             actions: [
-              sendTo("listener", ({ context, event }) => {
+              sendTo("listener", ({ event }) => {
                 return { type: "start", data: event.ref };
               }),
             ],
@@ -68,8 +116,18 @@ const hlsMachine = setup({
             target: "notSupported",
           },
         ],
+        MANIFEST_LOAD_ERROR: {
+          target: "retryManifest",
+        },
+        MANIFEST_LOAD_TIMEOUT: {
+          target: "retryManifest",
+        },
+        MANIFEST_PARSING_ERROR: {
+          target: "retryManifest",
+        },
       },
     },
+    retryManifest: {},
     notSupported: {
       type: "final",
     },
@@ -82,7 +140,7 @@ function App() {
   const [state, send] = useMachine(hlsMachine);
 
   useEffect(() => {
-    if (state.matches("idle") && videoRef.current) {
+    if (state.value === "idle" && videoRef.current) {
       send({ type: "SET_VIDEO_REF", ref: videoRef.current });
     }
   }, [state.value, videoRef, send]);
@@ -93,6 +151,12 @@ function App() {
         className="container"
         style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
       >
+        {state.matches("retryManifest") && (
+          <div className="loading-message">Offline</div>
+        )}
+        {state.matches("idle") && (
+          <div className="loading-message">Loading</div>
+        )}
         <video
           id="video"
           ref={videoRef}
